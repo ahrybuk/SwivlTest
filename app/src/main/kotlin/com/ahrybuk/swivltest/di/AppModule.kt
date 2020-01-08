c7ef4b1237747e2473dc43615b6cc3db866f2a72
Oleksandr Hrybuk <a.hrybuk@gmail.com>
2020-01-08 20:23:19 +0200

    add users repository to koin appModule

--- app/src/main/kotlin/com/ahrybuk/swivltest/di/AppModule.kt
+++ app/src/main/kotlin/com/ahrybuk/swivltest/di/AppModule.kt
@@ -1,9 +1,13 @@
 package com.ahrybuk.swivltest.di
 
 import com.ahrybuk.swivltest.api.configuration.RetrofitProvider
+import com.ahrybuk.swivltest.repositories.NetworkUsersRepository
+import com.ahrybuk.swivltest.repositories.UsersRepository
 import org.koin.dsl.module
 
 val appModule = module {
 
     single { RetrofitProvider().retrofit }
+
+    factory<UsersRepository> { NetworkUsersRepository(get()) }
 }
